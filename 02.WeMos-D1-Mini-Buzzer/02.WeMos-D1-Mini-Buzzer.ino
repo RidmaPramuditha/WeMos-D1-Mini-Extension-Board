9947bc427cfef0d80fa527545a92c6dfd986e534
Pramuditha <49787217+RidmaPramuditha@users.noreply.github.com>
2025-03-20 15:00:30 +0530

    WeMos D1 Mini buzzer
    WeMos D1 Mini buzzer

---
+++ 02.WeMos-D1-Mini-Buzzer/02.WeMos-D1-Mini-Buzzer.ino
@@ -0,0 +1,23 @@
+/*
+  WeMos D1 Mini buzzer
+
+  This example shows how to Blink LED
+  The circuit:
+  LED attached to WeMos D1 Mini Extension Board as follows:
+ ** Buzzer Positive - pin D5 (GPIO 14)
+
+  created   March 2025
+  by P.R. Pramuditha
+
+*/
+
+void setup() {
+  pinMode(14, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(14, HIGH);
+  delay(1000);
+  digitalWrite(14, LOW);
+  delay(1000);
+}
